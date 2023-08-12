6cd26bf3945fb878968ea877e010294c7cef2ddc
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-08-12 23:51:13 +0530

    virtual function is completed

---
+++ polymorphism/virtaul_func_ex.cpp
@@ -0,0 +1,25 @@
+//  example of the  virtual class function
+#include<iostream>
+using namespace std;
+// create the class 
+class BasicCar{
+    public :
+   virtual void start(){
+        cout<<" basic car is started "<<endl;
+    }
+};
+class Advanced :public BasicCar{
+    public:
+
+    void start(){
+        cout<<" advanced car is started "<<endl;
+    }
+};
+
+// main methods start here ....
+int main(){
+    //create the base class pointer and derived class object 
+    BasicCar *ptr = new Advanced();
+    ptr->start();
+    return 0;
+}
