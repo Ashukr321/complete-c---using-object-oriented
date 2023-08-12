be1857e1df5a2fb29f5e2111f3c85350891deb78
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-08-13 00:58:54 +0530

    user input is completed

---
+++ basic_concepts/user_input.cpp
@@ -0,0 +1,11 @@
+// here we see the user input in the cpp
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    cout<<"enter your number"<<endl;
+    cin>>n;
+    cout<<"enter your number :=> "<<n<<endl;
+    return 0; 
+}
