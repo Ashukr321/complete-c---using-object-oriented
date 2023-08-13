56c9e752716c6038973a1c2124b3ca274a48a731
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-08-13 08:09:56 +0530

    functio is completed

---
+++ basic_concepts/function/recursion.cpp
@@ -0,0 +1,5 @@
+// Recursion is the technique of making a function call itself. This technique provides a way to break complicated problems down into simple problems which are easier to solve.
+
+// Recursion may be a bit difficult to understand. The best way to figure out how it works is to experiment with it.
+
+//The developer should be very careful with recursion as it can be quite easy to slip into writing a function which never terminates, or one that uses excess amounts of memory or processor power. However, when written correctly recursion can be a very efficient and mathematically-elegant approach to programming.
