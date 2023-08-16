4b8512cf3717fcfb6db3295a08a7f213e2300499
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-08-16 19:15:39 +0530

    volume of sphare is completed

---
+++ oop_lab1/volume_sphere.cpp
@@ -0,0 +1,14 @@
+// volume of shpere in the cpp 
+#include<iostream>
+using namespace std;
+
+// main method start here 
+int main(){
+    double r , volume ;
+    cout<<"enter the radius of shpere "<<endl;
+    cin>>r;
+    volume = (4.0/3)*(22.0/7)*(r)*(r)*(r);
+    cout<<"volume of the sphare is : "<<volume<<endl;
+    system("pause");
+    return 0; 
+}
