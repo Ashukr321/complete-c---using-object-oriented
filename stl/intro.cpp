0fd24ae37657062cb7bc5a88f6d57671e835ba17
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-08-16 19:38:14 +0530

    intro of stl is completed

---
+++ stl/intro.cpp
@@ -0,0 +1,48 @@
+/*
+stl => structure templates libararies 
+
+classify in to 4 type 
+container 
+iterators 
+algorithms 
+function 
+
+container
+sequential 
+vector
+stack
+queue 
+pari not a container 
+
+// order container 
+map , multimap 
+sets 
+
+// unorder 
+unorder map 
+unorder set 
+
+
+// some important nested container 
+vector <vector<int>> 
+map <int , vector <int >>
+set <pair<int,string>>
+
+iterator / similar to the pointer 
+begin(),end()
+continuty 
+
+algorithms ⭐⭐⭐⭐⭐
+upper bound 
+lower bound 
+sort 
+max 
+min 
+accumulators 
+reverse 
+count 
+find 
+
+
+
+*/
