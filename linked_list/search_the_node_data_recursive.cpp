1b53f7c25469299069124e24d7cad8f42cf6be38
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-08-21 21:03:54 +0530

    find the node data in the linked list is complted

---
+++ linked_list/search_the_node_data_recursive.cpp
@@ -0,0 +1,101 @@
+//search in linked list 
+
+#include<iostream>
+using namespace std;
+
+//create the class of the node 
+class Node {
+    public:
+
+    int data ;
+    Node * next;
+
+    //CREATE the constrcutor 
+    Node (int data){
+        this->data = data;
+        this->next=NULL;
+    }
+};
+
+//create the linked list 
+Node * insertNodeAtEnd(){
+    cout<<"enter your data of the linked list "<<endl;
+    cout<<"enter the -1 for stop the taking the input "<<endl;
+    int data;
+    cin>>data;
+
+    //create two pointr head and tail 
+    Node * head = NULL;
+    Node * tail = NULL;
+
+    while (data !=-1)
+    {
+        //create the newNode 
+        Node * newNode = new Node(data);
+        if (head ==NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+        cin>>data;
+    }
+    return head;
+}
+
+//print the node 
+void printNode(Node * head ){
+    Node * temp = head;
+    while (temp!=NULL)
+    {
+        cout<<temp->data<<"  -> ";
+        temp = temp->next;
+    }
+    
+}
+
+// here we search the  data in the linked list if present then return the true else false 
+// bool isPresentInLinkedList(Node* head, int key ){
+    
+//     while (head!=NULL )
+//     {
+//         if (head->data == key)
+//         {
+//                 return true;
+//         }
+//         //move the head at next pointer 
+//         head = head->next;
+        
+//     }
+//     return false ;
+// }
+
+// recursive ways to search the data in the linked list 
+bool findElement(Node * head , int key){
+    if (head ==NULL)
+    {
+        return false;
+    }
+    if ((head)->data==key){
+        return true;
+    }else{
+        return   findElement((head)->next,key);//calling the recursion 
+    }
+    
+}
+
+// main method start here 
+int main(){
+    Node * head = insertNodeAtEnd();
+    cout<<"print the  data of the linked list "<<endl;
+    printNode(head);
+    cout<<"check the data is present or not"<<endl;
+    cout<<endl;
+    cout<<findElement(head,3)<<endl;// 1 yes , 0 for no 
+
+
+    return 0;
+}
