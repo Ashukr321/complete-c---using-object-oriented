6b611efeab5a89307f334b9878ff81020891d181
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-09-05 10:20:01 +0530

    human example is complted

---
+++ oops_low_level_design/ex1.cpp
@@ -0,0 +1,24 @@
+//  human example
+#include<iostream>
+using namespace std;
+
+class Human{
+    int age;
+    int weight ;
+    public:
+    void sleep(){
+        cout<<"He is sleaping "<<endl;
+
+    }
+    void eat(){
+        cout<<" He is eating"<<endl;
+    }
+};
+
+int main(){
+    //create the static object of the class 
+    Human m1;
+    m1.eat();
+    m1.sleep();
+    return 0;
+}
